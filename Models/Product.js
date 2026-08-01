4a0428c73831ec0b85054232410565338dbf7df0
gozeszn <preciousonyenaucheya2003@gmail.com>
2026-08-01 18:51:09 +0100

    creating the controller and routes

--- Models/Product.js
+++ Models/Product.js
@@ -1,35 +1,40 @@
-const mongoose = require('mongoose');
-const { timeStamp } = require('node:console');
-const { type } = require('node:os');
+const mongoose = require("mongoose");
+const { timeStamp } = require("node:console");
+const { type } = require("node:os");
 
 const ProductSchema = new mongoose.Schema(
-    {
-       name: {
-        type: String,
-        required: true
-       },
-       description: {
-        type: String,
-        required: true
-       },
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
 
-       price:{
-        type: Number,
-        required: true
-       },
+    price: {
+      type: Number,
+      required: true,
+    },
 
-       quantity: {
-        type: Number,
-        required: true
-       },
+    quantity: {
+      type: Number,
+      required: true,
+    },
 
-       size:{
-        type: String,
-        required: true
-       },
+    size: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+        required: false,
+  },
+   {timestamps: true} //date created and updated at}
+);
 
-       timestamps: true //date created and updated
-    
-});
+const Product = mongoose.model("Product", ProductSchema);
 
-const Product = mongoose.model('Product', ProductSchema);
+
+module.exports = Product; //exporting the Product model to be used in other parts of the application
